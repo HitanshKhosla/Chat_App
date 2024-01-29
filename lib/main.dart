8dd22a1a19809791122c9290f5ec57502db498a7
HitanshKhosla <hitanshkhosla2003@gmail.com>
2024-01-29 20:47:22 +0530

    Initial commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,19 @@
-import 'package:flash_chat/screens/registartaion_screen.dart';
-import 'package:flutter/material.dart';
-import 'package:flash_chat/screens/welcome_screen.dart';
-import 'package:flash_chat/screens/login_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flash_chat/screens/chat_screen.dart';
+import 'package:flash_chat/screens/login_screen.dart';
+import 'package:flash_chat/screens/registartaion_screen.dart';
+import 'package:flash_chat/screens/welcome_screen.dart';
+import 'package:flutter/material.dart';
 
+import 'firebase_options.dart';
 
-void main() => runApp(FlashChat());
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
+  runApp(FlashChat());
+}
 
 class FlashChat extends StatelessWidget {
   @override
